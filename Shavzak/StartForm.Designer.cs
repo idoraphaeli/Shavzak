83b27dcc02ca41e4b4fb12ddd93554ac7098ac08
Ido Raphaeli <ido010111@gmail.com>
2025-06-28 13:35:12 +0300

    Add files via upload

---
+++ Shavzak/StartForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Shavzak
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartForm));
+            this.btnSoldiers = new System.Windows.Forms.Button();
+            this.SoldierOptions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showSoldiersListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewSoldierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeSoldierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchSoldierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnMissions = new System.Windows.Forms.Button();
+            this.btnBuildShavzak = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.MissionOptions = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showMissionsListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewMissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeMissionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SoldiersPhoto = new System.Windows.Forms.PictureBox();
+            this.SoldierOptions.SuspendLayout();
+            this.MissionOptions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SoldiersPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSoldiers
+            // 
+            this.btnSoldiers.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSoldiers.ContextMenuStrip = this.SoldierOptions;
+            this.btnSoldiers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnSoldiers.Location = new System.Drawing.Point(110, 273);
+            this.btnSoldiers.Name = "btnSoldiers";
+            this.btnSoldiers.Size = new System.Drawing.Size(431, 50);
+            this.btnSoldiers.TabIndex = 0;
+            this.btnSoldiers.Text = "Soldiers";
+            this.btnSoldiers.UseVisualStyleBackColor = true;
+            this.btnSoldiers.Click += new System.EventHandler(this.btnSoldiers_Click);
+            // 
+            // SoldierOptions
+            // 
+            this.SoldierOptions.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.SoldierOptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showSoldiersListToolStripMenuItem,
+            this.addNewSoldierToolStripMenuItem,
+            this.removeSoldierToolStripMenuItem,
+            this.searchSoldierToolStripMenuItem});
+            this.SoldierOptions.Name = "contextMenuStrip1";
+            this.SoldierOptions.Size = new System.Drawing.Size(198, 100);
+            this.SoldierOptions.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // showSoldiersListToolStripMenuItem
+            // 
+            this.showSoldiersListToolStripMenuItem.AutoSize = false;
+            this.showSoldiersListToolStripMenuItem.Name = "showSoldiersListToolStripMenuItem";
+            this.showSoldiersListToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.showSoldiersListToolStripMenuItem.Text = "Show Soldiers List";
+            // 
+            // addNewSoldierToolStripMenuItem
+            // 
+            this.addNewSoldierToolStripMenuItem.AutoSize = false;
+            this.addNewSoldierToolStripMenuItem.Name = "addNewSoldierToolStripMenuItem";
+            this.addNewSoldierToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.addNewSoldierToolStripMenuItem.Text = "Add New Soldier";
+            this.addNewSoldierToolStripMenuItem.Click += new System.EventHandler(this.addNewSoldierToolStripMenuItem_Click);
+            // 
+            // removeSoldierToolStripMenuItem
+            // 
+            this.removeSoldierToolStripMenuItem.AutoSize = false;
+            this.removeSoldierToolStripMenuItem.Name = "removeSoldierToolStripMenuItem";
+            this.removeSoldierToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.removeSoldierToolStripMenuItem.Text = "Remove Soldier";
+            // 
+            // searchSoldierToolStripMenuItem
+            // 
+            this.searchSoldierToolStripMenuItem.AutoSize = false;
+            this.searchSoldierToolStripMenuItem.Name = "searchSoldierToolStripMenuItem";
+            this.searchSoldierToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.searchSoldierToolStripMenuItem.Text = "Search Soldier";
+            // 
+            // btnMissions
+            // 
+            this.btnMissions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMissions.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnMissions.Location = new System.Drawing.Point(110, 341);
+            this.btnMissions.Name = "btnMissions";
+            this.btnMissions.Size = new System.Drawing.Size(431, 50);
+            this.btnMissions.TabIndex = 1;
+            this.btnMissions.Text = "Missions";
+            this.btnMissions.UseVisualStyleBackColor = true;
+            this.btnMissions.Click += new System.EventHandler(this.btnMissions_Click);
+            // 
+            // btnBuildShavzak
+            // 
+            this.btnBuildShavzak.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuildShavzak.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnBuildShavzak.Location = new System.Drawing.Point(110, 204);
+            this.btnBuildShavzak.Name = "btnBuildShavzak";
+            this.btnBuildShavzak.Size = new System.Drawing.Size(431, 50);
+            this.btnBuildShavzak.TabIndex = 2;
+            this.btnBuildShavzak.Text = "Build Shavzak!";
+            this.btnBuildShavzak.UseVisualStyleBackColor = true;
+            this.btnBuildShavzak.Resize += new System.EventHandler(this.StartForm_Resize);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnDownload.Location = new System.Drawing.Point(110, 419);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(431, 50);
+            this.btnDownload.TabIndex = 3;
+            this.btnDownload.Text = "Download Status";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // MissionOptions
+            // 
+            this.MissionOptions.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MissionOptions.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showMissionsListToolStripMenuItem,
+            this.addNewMissionToolStripMenuItem,
+            this.removeMissionToolStripMenuItem,
+            this.searchToolStripMenuItem});
+            this.MissionOptions.Name = "MissionOptions";
+            this.MissionOptions.Size = new System.Drawing.Size(201, 100);
+            this.MissionOptions.Opening += new System.ComponentModel.CancelEventHandler(this.missionOption_Opening);
+            // 
+            // showMissionsListToolStripMenuItem
+            // 
+            this.showMissionsListToolStripMenuItem.Name = "showMissionsListToolStripMenuItem";
+            this.showMissionsListToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.showMissionsListToolStripMenuItem.Text = "Show Missions List";
+            // 
+            // addNewMissionToolStripMenuItem
+            // 
+            this.addNewMissionToolStripMenuItem.Name = "addNewMissionToolStripMenuItem";
+            this.addNewMissionToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.addNewMissionToolStripMenuItem.Text = "Add New Mission";
+            // 
+            // removeMissionToolStripMenuItem
+            // 
+            this.removeMissionToolStripMenuItem.Name = "removeMissionToolStripMenuItem";
+            this.removeMissionToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.removeMissionToolStripMenuItem.Text = "Remove Mission";
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(200, 24);
+            this.searchToolStripMenuItem.Text = "Search Mission";
+            this.searchToolStripMenuItem.Click += new System.EventHandler(this.searchToolStripMenuItem_Click);
+            // 
+            // SoldiersPhoto
+            // 
+            this.SoldiersPhoto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SoldiersPhoto.Image = ((System.Drawing.Image)(resources.GetObject("SoldiersPhoto.Image")));
+            this.SoldiersPhoto.Location = new System.Drawing.Point(0, 29);
+            this.SoldiersPhoto.Name = "SoldiersPhoto";
+            this.SoldiersPhoto.Size = new System.Drawing.Size(630, 220);
+            this.SoldiersPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SoldiersPhoto.TabIndex = 5;
+            this.SoldiersPhoto.TabStop = false;
+            this.SoldiersPhoto.Click += new System.EventHandler(this.SoldiersPhoto_Click);
+            this.SoldiersPhoto.Resize += new System.EventHandler(this.SoldiersPhoto_Resize);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 592);
+            this.Controls.Add(this.SoldiersPhoto);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnBuildShavzak);
+            this.Controls.Add(this.btnMissions);
+            this.Controls.Add(this.btnSoldiers);
+            this.Name = "StartForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.startForm_Load);
+            this.Resize += new System.EventHandler(this.StartForm_Resize);
+            this.SoldierOptions.ResumeLayout(false);
+            this.MissionOptions.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SoldiersPhoto)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSoldiers;
+        private System.Windows.Forms.Button btnMissions;
+        private System.Windows.Forms.Button btnBuildShavzak;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.ContextMenuStrip SoldierOptions;
+        private System.Windows.Forms.ToolStripMenuItem showSoldiersListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewSoldierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeSoldierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchSoldierToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip MissionOptions;
+        private System.Windows.Forms.ToolStripMenuItem showMissionsListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewMissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeMissionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.PictureBox SoldiersPhoto;
+    }
+}
---
+++ Shavzak/StartForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shavzak
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+            Text = "Shavzak";
+            this.Icon = new Icon("soldier-icon.ico");
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void StartForm_Resize(object sender, EventArgs e)
+        {
+            int btnSpace = 12; // Space between buttons
+            int btnNum = 4; // Number of buttons
+
+            btnBuildShavzak.Left = (this.ClientSize.Width - btnBuildShavzak.Width) / 2;
+            btnBuildShavzak.Top = (this.ClientSize.Height - btnBuildShavzak.Height) / 2;
+            btnBuildShavzak.Height = (this.ClientSize.Height - btnBuildShavzak.Top - btnNum * btnSpace)/ btnNum;
+            btnBuildShavzak.Width = (int)(this.ClientSize.Width * 0.7);
+
+            btnMissions.Left = btnBuildShavzak.Left;
+            btnMissions.Top = btnBuildShavzak.Top + btnBuildShavzak.Height + btnSpace;
+            btnMissions.Height = btnBuildShavzak.Height;
+            btnMissions.Width = btnBuildShavzak.Width;
+
+            btnSoldiers.Left = btnBuildShavzak.Left;
+            btnSoldiers.Top = btnBuildShavzak.Top + (btnBuildShavzak.Height + btnSpace) * 2;
+            btnSoldiers.Height = btnBuildShavzak.Height;
+            btnSoldiers.Width = btnBuildShavzak.Width;
+
+            btnDownload.Left = btnBuildShavzak.Left;
+            btnDownload.Top = btnBuildShavzak.Top + (btnBuildShavzak.Height + btnSpace) * 3;
+            btnDownload.Height = btnBuildShavzak.Height;
+            btnDownload.Width = btnBuildShavzak.Width;
+        }
+
+        private void startForm_Load(object sender, EventArgs e)
+        {
+            this.StartForm_Resize(sender, e);
+            this.MinimumSize = new Size(350, 470);
+        }
+
+        private void btnSoldiers_Click(object sender, EventArgs e)
+        {
+            int desiredWidth = btnSoldiers.Width;
+
+            foreach (ToolStripItem item in SoldierOptions.Items)
+            {
+                item.AutoSize = false;
+                item.Width = desiredWidth;
+            }
+
+            SoldierOptions.Show(btnSoldiers, new Point(0, btnSoldiers.Height));
+        }
+
+        private void addNewSoldierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            SoldierOptions.AutoSize = false;
+            SoldierOptions.Width = btnSoldiers.Width;
+
+            foreach (ToolStripItem item in SoldierOptions.Items)
+            {
+                item.AutoSize = false;
+                item.Width = SoldierOptions.Width;
+                item.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void missionOption_Opening(object sender, CancelEventArgs e)
+        {
+            MissionOptions.AutoSize = false;
+            MissionOptions.Width = btnSoldiers.Width;
+
+            foreach (ToolStripItem item in MissionOptions.Items)
+            {
+                item.AutoSize = false;
+                item.Width = MissionOptions.Width;
+                item.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MissionOptions_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void searchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMissions_Click(object sender, EventArgs e)
+        {
+            int desiredWidth = btnMissions.Width;
+
+            foreach (ToolStripItem item in MissionOptions.Items)
+            {
+                item.AutoSize = false;
+                item.Width = desiredWidth;
+            }
+
+            MissionOptions.Show(btnMissions, new Point(0, btnMissions.Height));
+        }
+
+        private void SoldiersPhoto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SoldiersPhoto_Resize(object sender, EventArgs e)
+        {
+            SoldiersPhoto.Width = this.ClientSize.Width;
+            SoldiersPhoto.Height = (int)(this.ClientSize.Height / 2.7);
+            SoldiersPhoto.Left = 0;
+            SoldiersPhoto.Top = 30;
+        }
+    }
+}
